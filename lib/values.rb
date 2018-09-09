6808543954988431f86f6523b44fead8ebcfefbb
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-09 16:10:09 +0000

    automatically backed up by learn

--- lib/values.rb
+++ lib/values.rb
@@ -10,13 +10,6 @@ require 'pry'
 
 def get_the_values(groceries)
 
-# groceries = {
-#   dairy: ["milk", "yogurt", "cheese"],
-#   vegetable: ["carrots", "broccoli", "cucumbers"],
-#   meat: ["chicken", "steak", "salmon"],
-#   grains: ["rice", "pasta"]
-# }
-
 groceries.values
 
 end
